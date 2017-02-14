62f6c107b14c1836493c746270ee976a3a90af08
babotaemin <babotaemin@nate.com>
2017-02-14 14:04:27 +0900

    config modify

--- app/config.js
+++ app/config.js
@@ -2,7 +2,7 @@
 
 angular.module('config', [])
     .constant('configValue', {
-        IMG_URL : 'app/assets/imgs/',
+        IMG_URL : 'assets/imgs/',
         MAP_INIT_LAT : 36.32087,
         MAP_INIT_LON : 128.056,
         MAP_INIT_ZOOM : 8
